e4e4de9e6de5ea66fc65d5f74c4bfd1d2143a80b
Andy Moreno <andymoreno.ing@gmail.com>
2021-05-28 21:01:29 -0500

    SE DOUCMENTO EL CONTROLADOR AUTHCONTROLLER Y EL MODELO USER

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,29 +5,32 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash as FacadesHash;
+use Illuminate\Support\Facades\Hash as FacadesHash; //PARA DESIFRAR LA CLAVE ENCRIPTADA
 use Iluminate\Http\Response;
 
 
 class AuthController extends Controller
 {
-    //
+    //METODO PARA REGISTRAR UN USUARIO
+    //USANDO LOS CAMPOS QUE SE ENCUENTAN EN LA MIGRACION USUARIOS
+    public function register(Request $request){
 
-    public function register( Request $request){
+        //SE VALIDARAN LOS CAMPOS CORRESPONDIENTES
         $campos = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|unique:users,email',
-            'password' => 'required|string|confirmed'
+            'name' => 'required|string', 
+            'email' => 'required|string|unique:users,email',// CAMPO REQUERIDO, QUE SEA UN STRING Y UE SEA UNICO Y SE COMPARARA CON EL CAMPO EMAIL
+            'password' => 'required|string|confirmed' //CAMPO REQUERIDO, ENTERO Y QUE SE CONFIRME CON EL CAMPO (password_confirmation)
             ]
         );
 
+        //SE REGISTRARA EL USUARIO ENVIADO POR METODO POST
         $usuario = User::create([
             'name' => $campos['name'],
             'email' => $campos['email'],
-            'password' => bcrypt($campos['password'])
+            'password' => bcrypt($campos['password'])// SE USA LA FUNCION bcrypt PARA ENCRIPTAR LA PASSWORD
         ]);
 
-        $token = $usuario->createToken('myapptoken')->plainTextToken;
+        $token = $usuario->createToken('myapptoken')->plainTextToken; // SE CREA EL TOKEN EN FORMATO PLANO
 
         $response = [
             'user' => $usuario,
@@ -37,15 +40,15 @@ class AuthController extends Controller
         return response($response,201);
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request){ //DESLOGEAR USUARIO 
         auth()->user()->tokens()->delete();
         return  [
             'mensaje' => 'Desconectado'
         ];
     }
 
-    public function login(Request $request){
-        $campos = $request->validate([
+    public function login(Request $request){//LOGUEAR USUARIO
+        $campos = $request->validate([ // SE VALIDAN LOS CMAPOS LOGIN Y PASSWORD
             'email' => 'required|string',
             'password' => 'required|string'
             ]
@@ -55,7 +58,7 @@ class AuthController extends Controller
         // se consulta la tabla user, el campo EMAIL y que busque el primer registro
         $usuario = User::where('email', $campos['email'])->first();
 
-        //chequea contrase;a
+        //chequea  SI SE CNSIGUIO EL EMAIL Y SI LO CONSIGUE CHEQUEA LA contrase;a
         if (!$usuario || !FacadesHash::check($campos['password'], $usuario->password)){
             return response([
                 'mensaje' => 'Usuario no Registrado'
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;// AGREGAR ESTA LIBRERIA PARA PODER ACCEDER A LOS TOKEN
 
 class User extends Authenticatable
 {
+    // PARA QUE USE LOS TOKENS SE DEVE AGREGAR (HasApiTokens)
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
